0e7226c72ddc037dec1652a31aeb48bac097a184
admin <shalindasilva1@gmail.com>
2020-05-26 23:19:12 +0530

    init

---
+++ secret.py
@@ -0,0 +1,7 @@
+#paste your api_hash and api_id from my.telegram.org
+api_hash = "9814d3d5eb9b22641e2cd2008e5f1ee9"
+api_id = 1233487
+entity = "teleDownloader"
+Invite_link = "AAAAAFAXbZf3paWP966D9w"
+channel_id = 1343712663
+channel_hash = -6276914499196237418
---
+++ telegram_create_session.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon import functions, types, utils
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+from secret import *
+
+def callback(current, total):
+    print('Downloaded', current, 'out of', total,
+          'bytes: {:.2%}'.format(current / total))
+
+with TelegramClient(entity, api_id, api_hash) as client:
+    message_id_list = []
+    my_channel = client.get_entity(PeerChannel(channel_id))
+    for message in client.iter_messages(my_channel):
+        message_id_list.append(message.id)
+    my_channel_message_list = client(functions.channels.GetMessagesRequest(
+        channel=my_channel,
+        id=message_id_list
+    )).messages
+    
+    for message in my_channel_message_list:
+        if type(message.media) != type(None):
+            file_name = message.media.document.attributes[0].file_name
+            client.download_media(message, progress_callback=callback, file='storage/'+file_name)
+            client(functions.channels.DeleteMessagesRequest(
+                channel = my_channel,
+                id = [message.id]
+            ))
+    
+    client.stop()
+    
